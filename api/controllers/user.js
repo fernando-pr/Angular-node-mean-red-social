01ee94b316dc0a15234152b2aa5cd1ce13b1cf20
fernando-pr <fernando321987@gmail.com>
2020-04-09 19:13:50 +0200

    Control de guardado de usuarios dublicados

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -28,22 +28,40 @@ function saveUser(req, res) {
         user.role = 'ROLE_USER';
         user.image = null;
 
-        bcrypt.hash(params.password, null, null, (err, hash) =>{
-            user.password = hash;
+        // Controlar usuarios duplicados
+        User.find({ $or: [ 
+            { email: user.email.toLowerCase() },
+            { nick: user.nick.toLowerCase() } 
+        ]}).exec((err, users) => {
+
+            if(err){return res.status(500).send({ message: "Error en la peticion de usuarios"}) }
+            
+            if(users && users.length >= 1) {
+                return res.status(200).send({ message: "El usuario que intenta registrar ya existe"});
+            } else {
+
+                // Cifra y guarda los datos
+                bcrypt.hash(params.password, null, null, (err, hash) =>{
+                    user.password = hash;
+                });
+
+                user.save((err, userStored) => {
+                    if(err) {
+                        return res.status(500).send({ message: "Error al guardar el usuario"});
+                    }
+
+                    if(userStored){
+                        res.status(200).send({user: userStored});
+
+                    } else{
+                        res.status(404).send({ message: "No se ha registrado el usuario"});
+                    }
+                })
+            }
+
         });
 
-        user.save((err, userStored) => {
-            if(err) {
-                return res.status(500).send({ message: "Error al guardar el usuario"});
-            }
-
-            if(userStored){
-                res.status(200).send({user: userStored});
-
-            } else{
-                res.status(404).send({ message: "No se ha registrado el usuario"});
-            }
-        })
+      
 
     } else {
         res.status(200).send({
