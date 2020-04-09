23ed834260b36bdcfb4c3f25f4cef05f2842dfea
fernando-pr <fernando321987@gmail.com>
2020-04-09 16:35:41 +0200

    creaci

---
+++ api/app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// Cargar Rutas
+
+// middlewares
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// Cors
+
+// Rutas
+app.get('/',(req, res) => {
+    res.status(200).send({
+        message: 'Hola mundo desde el servidor de nodejs'
+    })
+});
+
+app.get('/pruebas',(req, res) => {
+    res.status(200).send({
+        message: 'Accion de prueba en el servidor de nodejs'
+    })
+});
+
+
+// Export
+module.exports = app;
--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3800;
 
+//conexion database
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/curso_mean_social', { useNewUrlParser: true })
         .then(() =>  {
             console.log('Conectado correctamente a la base de datos curso_mean_social');
+            // Crear servidor
+            app.listen(port, () => {
+                console.log("Servidor Corriendo en localhost:3800");
+            });
         })
         .catch(err => console.log(err));
