16bc89dfd8165d69e8c440b86fb4d2a3d193594e
fernando-pr <fernando321987@gmail.com>
2020-04-09 20:27:13 +0200

    Metodo login en backend

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -70,9 +70,35 @@ function saveUser(req, res) {
     }
 }
 
+function loginUser(req, res) {
+    var params = req.body;
+
+    var email = params.email;
+    var password = params.password;
+
+    User.findOne({email: email}, (err, user) => {
+        if(err) {return res.status(500).send({message: "Error en la petición"})}
+   
+        if(user) {
+            bcrypt.compare(password, user.password, (err, check) => {
+                if(check) {
+                    //devolver datos del usuario
+                    return res.status(200).send({user});
+                } else {
+                    return res.status(404).send({message: "El usuario no se ha podido identificar"});
+                }
+
+            });
+        } else {
+            return res.status(404).send({message: "El usuario no se ha podido identificar o no existe"});
+        }
+    })
+}
+
 module.exports = {
     home,
     pruebas,
     saveUser,
+    loginUser,
 }
 
