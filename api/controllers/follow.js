271ad792b6e3a065820f5fac631a646eb9b3144d
fernando-pr <fernando321987@gmail.com>
2020-04-11 17:25:16 +0200

    Métodos para seguir y dejar de seguir a otro usuario

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -8,10 +8,35 @@ var User = require('../models/user');
 var Follow = require('../models/follow');
 
 
-function prueba(req, res) {
-    res.status(200).send({message: "Hola desde el controlador de follow"});
+function saveFollow(req, res) {
+    var params = req.body;
+    var follow = new Follow();
+    follow.user = req.user.sub;
+    follow.followed = params.followed;
+
+    follow.save((err, followStored) =>{
+        if (err) { return res.status(500).send({message: "Error al guardar el seguimiento"})}
+        
+        if (!followStored) {
+            return res.status(404).send({message: "El seguimiento no se ha guardado"});
+        }
+
+        res.status(200).send({followStored});
+    }); 
+}
+
+function deleteFollow(req, res) {
+    var userId = req.user.sub;
+    var followId = req.params.id;
+
+    Follow.find({'user' : userId, 'followed': followId}).remove(err => {
+        if (err) { return res.status(500).send({message: "Error al guardar el seguimiento"})}
+
+        return res.status(200).send({message: "El follow se ha eliminado"});
+    });
 }
 
 module.exports = {
-    prueba,
+    saveFollow,
+    deleteFollow,
 }
