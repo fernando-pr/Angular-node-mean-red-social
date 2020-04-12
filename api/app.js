d024185e4418a662d91d3f993b6522809900ca51
fernando-pr <fernando321987@gmail.com>
2020-04-12 18:02:15 +0200

    Carga inicial de publicaciones

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ var app = express();
 // Cargar Rutas
 var user_routes = require('./routes/user');
 var follow_routes = require('./routes/follow');
+var publication_routes = require('./routes/publication');
 
 // middlewares
 app.use(bodyParser.urlencoded({extended:false}));
@@ -18,7 +19,7 @@ app.use(bodyParser.json());
 // Rutas
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
-
+app.use('/api', publication_routes);
 
 // Export
 module.exports = app; 
