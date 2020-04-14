92ec6f82eb4d3280e7f0da424441dcc82947e0b7
fernando-pr <fernando321987@gmail.com>
2020-04-14 18:44:44 +0200

    Creacion de controlador, rutas y modelo para message

---
+++ api/controllers/message.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var moment = require('moment');
+var mongoosePaginate = require('mongoose-pagination');
+
+var User = require('../models/user');
+var Follow = require('../models/follow');
+var Message = require('../models/message');
+
+
+function probando(req, res) {
+    res.status(200).send({message: 'hola desde message'});
+}
+
+
+module.exports = {
+    probando,
+}
