a279c06c03e05393bde340bedf93ae6eba3200a0
fernando-pr <fernando321987@gmail.com>
2020-05-03 12:30:10 +0200

    Creación de mis datos

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { UserEditComponent } from './components/user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +18,7 @@ import { HomeComponent } from './components/home/home.component';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
+    UserEditComponent,
   ],
   imports: [
     BrowserModule,
