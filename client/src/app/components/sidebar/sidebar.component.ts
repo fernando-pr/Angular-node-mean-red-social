9c9da079f8c3b3697ec3f93f89e5961b6e1c4e57
fernando-pr <fernando321987@gmail.com>
2020-05-23 13:52:12 +0200

    Formulario de publicaciones

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { UserService } from "./../../services/user.service";
 import { GLOBAL } from '../../services/global';
+import { Publication } from "./../../models/publication";
 
 
 @Component({
@@ -15,7 +16,7 @@ export class SidebarComponent implements OnInit {
   public stats;
   public url;
   public status;
-
+  public publication: Publication;
   constructor(
     private _userService : UserService
   ) {
@@ -23,10 +24,16 @@ export class SidebarComponent implements OnInit {
     this.token = this._userService.getToken();
     this.stats = this._userService.getStats();
     this.url = GLOBAL.url;
+    this.publication = new Publication("","","","",this.identity._id);
   }
 
   ngOnInit() {
     console.log('sidebar cargado')
+  }
+
+  onSubmit(){
+    console.log(this.publication);
+
   }
 
 }
